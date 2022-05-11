f450d2f263a7f5032047fb966da1e9306e56d37f
Jonathan Assunção <62725742+Jhow88@users.noreply.github.com>
2022-05-11 16:50:51 -0300

    Condicional Simples
    Aulas de Eventos DOM e Condicional Simples

---
+++ Aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.5
+console.log (`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log ('Dirija sempre com cinto de segurança!')
