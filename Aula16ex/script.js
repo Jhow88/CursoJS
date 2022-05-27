20e1cb64cbaf4fb5b504087327eafafddd06ce90
Jonathan Assunção <62725742+Jhow88@users.noreply.github.com>
2022-05-27 18:21:30 -0300

    Exercicios
    Finalização do exercício da Aula 16 e Introdução a Objetos da Aula 17

--- Aula16ex/script.js
+++ Aula16ex/script.js
@@ -1,42 +1,66 @@
 let num = document.getElementById('fnum')
-    let res = document.getElementById('res')
-    let form = document.getElementById ('flist')
-    let valores = []
+let lista = document.getElementById('flist')
+let res = document.getElementById('res')
+let valores = []
+
+function isNumero(n) {
+if (Number(n) >= 1 && Number(n) <= 100) {
+    return true
+}else{
+    return false
+}
+}
+
+function inLista(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+    
+}
+    
 
 function adicionar() {
-    
-    if (num.value.length  == 0 || num.value > 100 ) {
-        window.alert('[Erro]')
-        
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `O valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML = ''//para quando for incluído mais valores após clicar finalizar, limpar novamente.   
     }else{
-
-        let n = Number(num.value)
-        var item = document.createElement('option')
-        form.appendChild(item)
-        item.text = `${n}`
-        }
-        
-
-   }
-   res.innerHTML =  `Ao todo temos ${n.lenght} números cadastrados <br>`
-   /*
-    function isNum(params) {
-        
-    }
-    function inList(params) {
-        if (n, l) {
-            
-        }
-        
+        window.alert('[Erro] Valor inválido ou já encontrado na lista!')
     }
 
+    num.value = ''
+    num.focus()  
+}
 
-       
-/*
-        var item = document.createElement('option')
-        tab.appendChild(item)
-        item.text = `${(n[item])}` res.innerHTML =  `Ao todo temos ${n.lenght} números cadastrados <br>`
-res.innerHTML += `O maior valor informado foi x <br>`
-res.innerHTML += `O menor valor informado foi x <br>`
-res.innerHTML += `Somando todos os valores, temos x <br>`
-res.innerHTML += `Somando todos os valores, temos x`*/
+    function finalizar() {
+        if (valores.length == 0) {
+            window.alert('Adicione valores antes de finalizar!')    
+        }else{
+    let tot = valores.length
+    let maior = valores[0]
+    let menor = valores [0]
+    let soma = 0
+    let media = 0        
+
+    for(let pos in valores){
+        soma += valores[pos]
+    if (valores[pos] > maior)    
+        maior = valores[pos]
+    if (valores[pos] < menor)
+        menor = valores[pos]    
+    }
+    media = soma / tot
+
+        res.innerHTML = ''
+        res.innerHTML +=  `Ao todo temos ${tot} números cadastrados<br> `
+        res.innerHTML += `O maior valor informado foi ${maior} <br>`
+        res.innerHTML += `O menor valor informado foi ${menor}<br>`
+        res.innerHTML += `Somando todos os valores, temos ${soma} <br>`
+        res.innerHTML += `A média dos valores digitados é ${media}<br>`
+    }
+    
+}
